4803320a31b9dd4af217680e1011d9a064cab3b3
Yehor Chervony <chervony.egor@gmail.com>
2023-04-06 19:04:02 +0300

    Доповнено заголовковий файл прототипами функцій до задач 9.1-9.3

---
+++ lab09/prj/ModulesChervonyi.h
@@ -0,0 +1,8 @@
+﻿#ifndef MODULESCHERVONYI_H_INCLUDED
+#define MODULESCHERVONYI_H_INCLUDED
+
+float s_calculation(float x, float y, float z);
+std::string task9_1(int point);
+float task9_2(float footsize);
+int task9_3(int number);
+#endif // MODULESCHERVONYI_H_INCLUDED
