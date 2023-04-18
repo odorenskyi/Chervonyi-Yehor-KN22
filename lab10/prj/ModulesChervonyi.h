e9e69bb581f136522542da2361515475b67d0048
Yehor Chervony <chervony.egor@gmail.com>
2023-04-18 23:16:07 +0300

    Додано заголовковий файл ModulesChervonyi

---
+++ lab10/prj/ModulesChervonyi.h
@@ -0,0 +1,15 @@
+﻿#ifndef MODULESCHERVONYI_H_INCLUDED
+#define MODULESCHERVONYI_H_INCLUDED
+
+float s_calculation(float x, float y, float z);
+
+std::string task9_1(int point);
+float task9_2(float footsize);
+int task9_3(int number);
+
+void task10_1(std::string ofname, std::string ifname);
+void task10_2(std::string fname);
+void task10_3(std::string fname, float x, float y, float z, int b);
+
+
+#endif // MODULESCHERVONYI_H_INCLUDED
